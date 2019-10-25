babc96b57a6b25b70406556388ce61312a03e456
Ted Hille <ted@galois.com>
2019-10-25 13:06:11 -0700

    plug redux-saga into state

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -1,12 +1,16 @@
 import {
+    applyMiddleware,
     combineReducers,
     createStore
 } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+
 
 import { reducer as system, ISystemState } from './system';
 import { reducer as testResults, ITestResultsState } from './test-results';
 import { reducer as ui, IUiState } from './ui';
+import { rootSaga } from './sagas';
 
 export interface IState {
     system: ISystemState,
@@ -20,7 +24,11 @@ const rootReducer = combineReducers({
     ui,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 export const store = createStore(
     rootReducer,
-    composeWithDevTools(),
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
+
+sagaMiddleware.run(rootSaga);
