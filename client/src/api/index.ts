2250278f02e13a12cb29eecde4e18bb6eea66105
Ted Hille <ted@galois.com>
2019-10-26 09:24:21 -0700

    api call to upload configurator model

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -9,7 +9,6 @@ export interface IConfigurator {
 }
 
 export const fetchConfigurators = async () => {
-
     return axios.request({
         url: '/configurator/list_db_models/',
         method: 'get',
@@ -20,3 +19,16 @@ export const fetchConfigurators = async () => {
     })
     .then(response => response.data);
 };
+
+export const submitConfigurator = async (systemName: string, systemJsonAsString: string) => {
+    return axios.request({
+        url: `/configurator/upload/${systemName}/global_var_cpu`,
+        method: 'post',
+        headers: {
+            'content-type': 'application/json',
+            accept: 'application/json',
+        },
+        data: systemJsonAsString,
+    })
+    .then(response => response.data);
+}
