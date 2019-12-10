8d71335701a50f1da31d269faec5949dfbc8eca8
Ted Hille <ted@galois.com>
2019-12-09 16:16:56 -0800

    update test script to use new test-data and run sample query

--- server/db/simple-test.py
+++ server/db/simple-test.py
@@ -1,32 +1,82 @@
-import sqlite3
+import csv, sqlite3
+from sqlalchemy import (
+    create_engine,
+    select,
+    text,
+    Table,
+    MetaData
+)
 
-# db_conn_str = './test.db'
-db_conn_str = ':memory:'
-conn = sqlite3.connect(db_conn_str)
-c = conn.cursor()
+def create_db(conn):
+    with open('./schema.sql', 'r') as f:
+        schema = f.read()
+        conn.executescript(schema)
+        conn.commit()
 
-try:
-    print('loading schema...')
-    with open('./schema.sql') as f:
-        db_schema = f.read()
-        c.executescript(db_schema)
+def load_csv(conn, table_name, csv_filename):
+    print(csv.list_dialects())
+    with open(f'./test-data/{csv_filename}.csv', 'r') as f:
+        dr = csv.DictReader(f, quoting=csv.QUOTE_NONNUMERIC)
+        fieldnames = ','.join(dr.fieldnames)
+        placeholders = ','.join(['?' for f in dr.fieldnames])
+        for i in dr:
+            data = [d for d in i.values()]
+            print(data)
+            conn.execute(f'INSERT INTO {table_name} ({fieldnames}) VALUES ({placeholders});', data)
+            conn.commit()
+
+def run_query(conn, query):
+    results = conn.execute(query)
+
+    for row in results:
+        print(row)
     
-    print('loading data...')
-    with open('./test-data.sql') as f:
-        db_inserts = f.read()
-        c.executescript(db_inserts)
-    
-    print('running sample queries...')
-    with open('./sample-queries.sql') as f:
-        db_queries = f.read().splitlines()
-        for query in db_queries:
-            if query.startswith('--'):
-                print('running queries "' + query.replace('--', '') + '" queries')
-            elif not query.isspace():
-                c.execute(query)
-                for row in c.fetchall():
-                    print(row)
-except Exception as err:
-    print('Unexpected error:', err)
-finally:
-    conn.close()
+    results.close()
+
+def inspect_table(meta, engine, conn, table_name):
+    try:
+        table_def = Table(table_name, meta, autoload=True, autoload_with=engine)
+        query = select([table_def])
+        run_query(conn, query)
+    except Exception as inst:
+        print(inst)
+
+# change the ':memory:' to a file if you'd like to keep the data around for further testing
+engine = create_engine(f'sqlite:///:memory:', echo=True)
+conn = engine.connect()
+meta = MetaData()
+
+table_names = [
+    'jobStatus',
+    'versionedResources',
+    'versionedResourceType',
+    'architectureModels',
+    'featureModels',
+    'featureConfigurations',
+    'vulnerabilityConfigurations',
+    'systemConfigurations',
+    'testRuns',
+    'jobStatus',
+    'jobs'
+]
+
+connection = conn.connection
+
+create_db(connection)
+load_csv(connection, 'jobs', 'job')
+load_csv(connection, 'versionedResources', 'versioned-resource')
+load_csv(connection, 'architectureModels', 'arch-model')
+load_csv(connection, 'architectureExtractionJobs', 'arch-job')
+load_csv(connection, 'featureModels', 'feat-model')
+load_csv(connection, 'featureExtractionJobs', 'feat-model-job')
+load_csv(connection, 'systemConfigurations', 'sys-config')
+load_csv(connection, 'systemConfigurationJobs', 'sys-job')
+
+for table_name in table_names:
+    inspect_table(meta, engine, conn, table_name)
+
+with open('./test-queries/fetch-sys-config-grid-data.sql') as f:
+    query = f.read()
+    result = conn.execute(text(query))
+    for row in result:
+        print(row)
