252b2a8ec71695af9a30338ce512b71d4c1fd2df
Ted Hille <ted@galois.com>
2020-01-04 07:39:53 -0800

    improve route definitions for versioned resources and utilize new resource-type relationship in the model

--- server/app/api/versioned_resource.py
+++ server/app/api/versioned_resource.py
@@ -10,6 +10,17 @@ ns = api.namespace(
     description='Operations on versioned resources'
 )
 
+versioned_resource_type = api.model('VersionedResourceType', {
+    'label': fields.String(
+        required=True,
+        description='Human-readable resource type label',
+    ),
+    'resourceTypeId': fields.Integer(
+        required=True,
+        description='Id of resource type',
+    ),
+})
+
 new_versioned_resource = api.model('NewVersionedResource', {
     'label': fields.String(
         required=True,
@@ -20,7 +31,8 @@ new_versioned_resource = api.model('NewVersionedResource', {
     'version': fields.String(
         required=True,
         description='Revision of resource'),
-    'type': fields.String(
+    'resourceType': fields.Nested(
+        versioned_resource_type,
         required=True,
         description='Label for the versioned resource type categorizing this resource'  # noqa E501
     )
@@ -39,10 +51,13 @@ existing_versioned_resource = api.inherit(
 
 
 @ns.route('')
-@ns.route('/<int:resourceId>')
-class VersionedResource(Resource):
-    @ns.doc('create a versioned resource')
+class VersionedResourceList(Resource):
     @ns.marshal_list_with(existing_versioned_resource)
+    def get(self):
+        current_app.logger.debug(f'fetching all versioned resources')
+        return VersionedResources.query.all()
+
+    @ns.marshal_with(existing_versioned_resource)
     @ns.expect(new_versioned_resource)
     def post(self):
         resource_input = api.payload
@@ -58,8 +73,28 @@ class VersionedResource(Resource):
 
         return new_resource
 
+
+@ns.route('/<int:resourceId>')
+class VersionedResource(Resource):
+    @ns.doc('create a versioned resource')
+    @ns.marshal_list_with(existing_versioned_resource)
+    @ns.expect(new_versioned_resource)
+    def put(self, resourceId):
+        current_app.logger.debug(f'updating resourceId: {resourceId}')
+        resource_input = api.payload
+        resource_type = VersionedResourceTypes.query.filter_by(label=resource_input['type']).first()  # noqa E501
+        existing_resource = VersionedResources.query.get_or_404(resourceId)
+        existing_resource.label = resource_input['label']
+        existing_resource.url = resource_input['url']
+        existing_resource.version = resource_input['version']
+        existing_resource.resourceTypeId = resource_type.resourceTypeId
+        db.session.add(existing_resource)
+        db.session.commit()
+
+        return existing_resource
+
     @ns.doc('fetch a versioned resource')
     @ns.marshal_with(existing_versioned_resource)
     def get(self, resourceId):
-        current_app.logger.debug(f'resourceId: {resourceId}')
-        return VersionedResources.query.filter_by(resourceId=resourceId).first()  # noqa E501
+        current_app.logger.debug(f'fetching resourceId: {resourceId}')
+        return VersionedResources.query.get(resourceId)
