b2f3dc0c5a142d7d2f3bcd4925c37721c1ee3a89
Ted Hille <ted@galois.com>
2020-01-12 10:26:18 -0800

    WIP: first pass at a "workflow" UX
    This is a fairly big commit focused on suggesting a potential UX for communicating the testing workflow as succinctly as possible to the user. Rather than reading the code, this should be reviewed by starting up the UI and exploring the actual, rendered page.
    
    Once the UX is settled on, there will be work to both clean up the code as well as implement actual state-management and the underlying API to support the data used in this page.

--- client/src/pages/Overview.test.tsx
+++ client/src/pages/Overview.test.tsx
@@ -1,66 +1,170 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { MemoryRouter } from 'react-router';
+import { mount } from 'enzyme';
 
-import { Overview } from './Overview';
-import { ISystemEntry } from '../state/system';
+import {
+    Overview,
+    IOverviewProps,
+    IWorkflow,
+} from './Overview';
+import {
+    SystemActionTypes,
+} from '../state/system';
+import { LoadingIndicator } from '../components/LoadingIndicator';
 
+const TWO_DAYS_IN_MS = 1000 * 60 * 60 * 24 * 2;
+export const TEST_WORKFLOWS: IWorkflow[] = [
+  {
+    id: 1,
+    label: 'Completed Workflow',
+    createdAt: (new Date(Date.now() - TWO_DAYS_IN_MS)).toISOString(),
+    updatedAt: (new Date(Date.now() - .5 * TWO_DAYS_IN_MS)).toISOString(),
+    systemConfig: { id: 2 },
+    testConfig: { id: 2 },
+    report: { id: 2 },
+  },
+  {
+    id: 2,
+    label: 'Failed build workflow',
+    createdAt: (new Date(Date.now() - 2 * TWO_DAYS_IN_MS)).toISOString(),
+    updatedAt: (new Date(Date.now() - 1.5 * TWO_DAYS_IN_MS)).toISOString(),
+    hasError: true,
+    systemConfig: {
+      id: 1,
+    },
+    testConfig: {
+      id: 1,
+    },
+    report: {
+      id: 1,
+      error: {
+        id: 1,
+        message: 'Something went wrong building the system',
+      },
+    }
+  },
+  {
+    id: 3,
+    label: 'Failed test config workflow',
+    createdAt: (new Date(Date.now() - 2 * TWO_DAYS_IN_MS)).toISOString(),
+    updatedAt: (new Date(Date.now() - 1.5 * TWO_DAYS_IN_MS)).toISOString(),
+    hasError: true,
+    systemConfig: { id: 1 },
+    testConfig: {
+      id: 1,
+      error: {
+        id: 1,
+        message: 'Something went wrong configuring the tests',
+      },
+    },
+  },
+  {
+    id: 4,
+    label: 'Missing Test Config Workflow',
+    createdAt: (new Date(Date.now() - TWO_DAYS_IN_MS)).toISOString(),
+    updatedAt: (new Date(Date.now() - .25 * TWO_DAYS_IN_MS)).toISOString(),
+    systemConfig: { id: 1 },
+  },
+  {
+    id: 5,
+    label: 'New Workflow',
+    createdAt: (new Date(Date.now())).toISOString(),
+  },
+];
 const genDate = (): string => {
-    return new Date(Date.now()).toISOString();
+    return (new Date(Date.now())).toISOString();
+};
+
+const genOverviewWrapper = (propsOverrides: Partial<IOverviewProps> = {}): ReturnType<typeof mount> => {
+    const props: IOverviewProps = {
+        workflows: [],
+        createNewWorkflow: jest.fn(),
+        isLoading: false,
+        dataRequested: true,
+        ...propsOverrides,
+    };
+
+    return mount(
+        <Overview {...props} />
+    );
 };
 
 describe('Overview', () => {
 
   it('renders without crashing', () => {
-    const wrapper = shallow(
-      <MemoryRouter initialEntries={['/overview']}>
-        <Overview systems={[]}/>
-      </MemoryRouter>
-    );
+    const wrapper = genOverviewWrapper();
 
-    expect(wrapper.find(Overview)).toHaveLength(1);
+    expect(wrapper.html()).toBeTruthy();
   });
 
-  it('renders the systems passed in', () => {
-    const systems: ISystemEntry[] = [
-      {
-        uid: 'TEST-HASH-1',
-        createdAt: genDate(),
-        lastUpdate: genDate(),
-        filename: 'TEST-FILE.fm.json',
-        featureCount: 5,
-        configs: [],
-        conftree: {
-          constraints: [],
-          features: {},
-          roots: [],
-          version: { base: 1 },
-        },
-        selectionUndos: [],
+  it('renders the workflows passed in', () => {
+    const workflows: IWorkflow[] = [
+        {
+          id: 1,
+          label: 'TEST WORKFLOW ONE',
+          createdAt: genDate(),
       },
       {
-        uid: 'TEST-HASH-2',
-        createdAt: genDate(),
-        lastUpdate: genDate(),
-        filename: 'TEST-FILE2.fm.json',
-        featureCount: 4,
-        configs: [],
-        conftree: {
-          constraints: [],
-          features: {},
-          roots: [],
-          version: { base: 1 },
-        },
-        selectionUndos: [],
+          id: 2,
+          label: 'TEST WORKFLOW TWO',
+          createdAt: genDate(),
       },
     ];
 
-    const wrapper = shallow(
-      <MemoryRouter initialEntries={['/overview']}>
-        <Overview systems={ systems } />
-      </MemoryRouter>
-    );
+    const wrapper = genOverviewWrapper({ workflows });
 
-    expect(wrapper.find(Overview).shallow().find('.system-row')).toHaveLength(2);
+    expect(wrapper.find('.workflow-row')).toHaveLength(2);
+  });
+
+  it('renders button to create new workflow', () => {
+    const wrapper = genOverviewWrapper();
+
+    expect(wrapper.find('button.new-workflow').length).toEqual(1);
+  });
+
+  it('renders loading indicator if we are loading', () => {
+    const wrapper = genOverviewWrapper({ isLoading: true });
+
+    expect(wrapper.find(LoadingIndicator).length).toEqual(1);
+  });
+
+  it('dispatches action to fetch data if no data has been fetched', () => {
+      const dispatchSpy = jest.fn();
+      const _wrapper = genOverviewWrapper({ dataRequested: false, dispatch: dispatchSpy });
+
+      expect(dispatchSpy).toHaveBeenCalledWith({ type: SystemActionTypes.FETCH_TEST_SYSTEMS });
+  });
+
+  it('does not show "new workflow" modal by default', () => {
+    const wrapper = genOverviewWrapper();
+
+    // this is weird, but apparently mount does some rendering of the modal
+    // even though the real-world does...
+    const editorContent = wrapper.find('.workflow-editor').map(f => f.text()).filter(f => f).length
+    expect(editorContent).toBe(0);
+  });
+
+  it('does show "new workflow" modal when the "create" button is clicked', () => {
+    const wrapper = genOverviewWrapper();
+
+    wrapper.find('button.new-workflow').simulate('click');
+
+    // this is weird, but apparently mount does some rendering of the modal
+    // even though the real-world does...
+    const editorContent = wrapper.find('.workflow-editor').map(f => f.text()).filter(f => f).length
+    expect(editorContent).toBeGreaterThan(0);
+  });
+
+  it('calls our "create" handler with content when the create button is clicked', () => {
+    const TEST_LABEL = 'TEST-LABEL';
+    const createNewWorkflowSpy = jest.fn();
+    const wrapper = genOverviewWrapper({
+        createNewWorkflow: createNewWorkflowSpy,
+    });
+
+    wrapper.find('button.new-workflow').simulate('click');
+    wrapper.find('input.new-workflow-label').simulate('blur', { target: { value: TEST_LABEL } });
+    wrapper.find('button.create-new-workflow').simulate('click');
+
+    expect(createNewWorkflowSpy).toHaveBeenCalledWith(TEST_LABEL);
   });
 });
--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux'
 
 import {
   Button,
+  ButtonGroup,
+  Col,
   Container,
+  Form,
+  Modal,
+  OverlayTrigger,
+  Row,
   Table,
+  Popover,
+  PopoverProps,
 } from 'react-bootstrap';
 
 import { IState } from '../state';
 
 import {
-  getSystems,
-  ISystemEntry,
   ISystemAction,
   fetchSystems,
 } from '../state/system';
@@ -26,58 +32,218 @@ import { Header } from '../components/Header';
 import { LoadingIndicator } from '../components/LoadingIndicator';
 
 import '../style/Overview.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+// NOTE: We currently are defining our workflow data-types here so
+//       we can easily iterate on the UX.
+//       Once we have settled on the UX, we need to create a new
+//       "workflow" state definition and move these to that definition.
+export interface IWorkflowError {
+  id: number;
+  message: string;
+}
+
+export interface IConfig {
+  id: number;
+  error?: IWorkflowError;
+}
+
+export interface IWorkflow {
+  id: number;
+  label: string;
+  createdAt: string;
+  hasError?: boolean;
+  updatedAt?: string;
+  systemConfig?: IConfig;
+  testConfig?: IConfig;
+  report?: IConfig;
+}
+
+// NOTE: Rendering buttons that represent a directed workflow involves
+//       a fair amount of logic to map the state of a given workflow.
+//       Right now we define all the types/components here, but we
+//       should consider pulling them out into their own "components"
+//       files once this UX is solidified
+interface IWorkflowProps {
+  workflowId?: number;
+  config?: IConfig;
+  disabled?: boolean;
+}
+
+interface IViewCreateButtonProps extends IWorkflowProps {
+  label: string;
+  path: string;
+  noNextStep?: boolean;
+}
+
+interface IWorkflowButton {
+  url: string;
+  label: string;
+  variant?: 'success' | 'danger';
+  disabled?: boolean;
+  noNextStep?: boolean;
+  tooltipError?: string;
+}
+
+interface IErrorTooltipProps extends PopoverProps {
+  label: string;
+}
+
+const WorkflowButton: React.FC<IWorkflowButton> = ({ url, label, variant, disabled, noNextStep, tooltipError }) => {
+  const renderTooltip: React.FC<IErrorTooltipProps> = (props) => (<Popover {...props} content={true}>{props.label}</Popover>);
+  const variantType = (disabled ? 'secondary' : variant) || 'primary';
+
+  return tooltipError ?
+    <OverlayTrigger placement='bottom' overlay={(props: IErrorTooltipProps) => renderTooltip({ ...props, label: tooltipError })}>
+      <Button disabled={disabled} variant='danger' href={url}>{label}</Button>
+    </OverlayTrigger>
+    : (
+      <Button disabled={disabled} variant={variantType} href={url}>
+        {label}
+        {!disabled && variantType !== 'primary' && !noNextStep && <FontAwesomeIcon icon={faChevronRight} />
+        }</Button>
+    );
+};
+
+const ViewCreateButton: React.FC<IViewCreateButtonProps> = ({ workflowId, config, path, disabled, label, noNextStep }) => {
+  if (config) {
+    return <WorkflowButton
+      disabled={disabled}
+      label={label}
+      noNextStep={noNextStep}
+      tooltipError={config.error && config.error.message}
+      url={`${path}/view/${config.id}`}
+      variant='success'
+    />;
+  } else {
+    return <WorkflowButton label={label} url={`${path}/create/${workflowId}`} disabled={disabled} noNextStep={noNextStep} />;
+  }
+};
+
+const SystemConfigButton: React.FC<IWorkflowProps> = ({ workflowId, config, disabled }) => {
+  return <ViewCreateButton label='System' path='/system-configuration' workflowId={workflowId} config={config} disabled={disabled} />;
+};
+
+const TestConfigButton: React.FC<IWorkflowProps> = ({ workflowId, config, disabled }) => {
+  return <ViewCreateButton label='Test' path='/test-configuration' workflowId={workflowId} config={config} disabled={disabled} />;
+};
+
+const ReportButton: React.FC<IWorkflowProps> = ({ workflowId, config, disabled }) => {
+  const label = config && !config.error ? 'View' : 'Build/Run';
+  return <ViewCreateButton label={label} path='/report' workflowId={workflowId} config={config} disabled={disabled} noNextStep={true} />;
+};
+
+const reportShouldBeDisabled = (workflow: IWorkflow): boolean => {
+  if (!workflow.systemConfig) return true;
+  if (workflow.systemConfig.error) return true;
+  if (workflow.systemConfig && !workflow.testConfig) return true;
+  if (workflow.testConfig && workflow.testConfig.error) return true;
+
+  return false;
+};
 
 export interface IOverviewProps {
   dispatch?: Dispatch<ISystemAction>;
-  systems?: ISystemEntry[];
-  isLoading?: boolean;
-  dataRequested?: boolean;
+  workflows: IWorkflow[];
+  isLoading: boolean;
+  dataRequested: boolean;
+  createNewWorkflow: (_: string) => void;
 }
 
-export const Overview: React.FC<IOverviewProps> = ({ dataRequested, dispatch, isLoading, systems }) => {
+// TODO: this is a placeholder implementation to allow us to 
+//       iterate on the UX before plumbing in the actual state
+const tmpWorkFlowFn = (label: string) => {
+  console.log(`create new workflow labeled "${label}"`);
+};
+
+export const Overview: React.FC<IOverviewProps> = ({
+  dataRequested,
+  dispatch,
+  isLoading,
+  workflows,
+  createNewWorkflow = tmpWorkFlowFn
+}) => {
 
   // useEffect is a way for us to trigger side-effects to load systems
   useEffect(() => {
     dataRequested || (dispatch && dispatch(fetchSystems()));
   });
 
+  const [newWorkflow, setNewWorkflow] = useState('');
+  const [showWorkflowEditor, setShowWorkflowEditor] = useState(false);
+
   return (
     <Container className='Overview'>
       <Header />
-      { isLoading && <LoadingIndicator /> }
+      {isLoading && <LoadingIndicator />}
+      <Modal className='workflow-editor' show={showWorkflowEditor} onHide={() => setShowWorkflowEditor(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create New Workflow</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group as={Row}>
+              <Form.Label column sm={2}>Name</Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  className='new-workflow-label'
+                  type='input'
+                  onBlur={(e: React.ChangeEvent<HTMLInputElement>) => setNewWorkflow(e.target.value)} />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={() => setShowWorkflowEditor(false)}>Cancel</Button>
+          <Button className='create-new-workflow' variant='primary' onClick={() => createNewWorkflow(newWorkflow)}>Create</Button>
+        </Modal.Footer>
+      </Modal>
       <h1>Overview</h1>
+      <Container className='overview-header'>
+        <Button className='new-workflow' onClick={() => setShowWorkflowEditor(true)}>New Workflow</Button>
+      </Container>
       <Table striped bordered hover variant='light'>
-            <thead>
-                <tr>
-                    <th>Hash</th>
-                    <th>Created At</th>
-                    <th>Last Updated</th>
-                    <th># of Features</th>
-                </tr>
-            </thead>
-            <tbody>
-                { systems && systems.map((s, i) => (
-                    <tr className='system-row' key={ `system-${i}`}>
-                        <td>{ s.uid }</td>
-                        <td>{ s.createdAt }</td>
-                        <td>{ s.lastUpdate }</td>
-                        <td>{ s.featureCount }</td>
-                        <td>
-                          <Button href={ `/configure-cpu/${s.uid}` }>Configure</Button>
-                        </td>
-                    </tr>
-                )) }
-            </tbody>
-        </Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Label</th>
+            <th>Created At</th>
+            <th>Last Updated</th>
+            <th>Configurations</th>
+            <th>Report</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workflows && workflows.map((w, i) => (
+            <tr className='workflow-row' key={`workflow-${i}`}>
+              <td>{w.id}</td>
+              <td className='label'>{w.label}</td>
+              <td>{w.createdAt}</td>
+              <td>{w.updatedAt || ''}</td>
+              <td>
+                <ButtonGroup>
+                  <SystemConfigButton workflowId={w.id} config={w.systemConfig} />
+                  <TestConfigButton workflowId={w.id} config={w.testConfig} disabled={!w.systemConfig} />
+                </ButtonGroup>
+              </td>
+              <td><ReportButton disabled={reportShouldBeDisabled(w)} workflowId={w.id} config={w.report} /></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
-}
+};
 
-const mapStateToProps = (state: IState): Partial<IOverviewProps> => {
+import { TEST_WORKFLOWS } from './Overview.test';
+
+const mapStateToProps = (state: IState): IOverviewProps => {
   return {
     dataRequested: getDataRequested(state),
     isLoading: getIsLoading(state),
-    systems: Object.values(getSystems(state)),
+    workflows: TEST_WORKFLOWS, // PLACEHOLDER...
+    createNewWorkflow: (_: string) => { }, // PLACEHOLDER...
   };
 };
 
