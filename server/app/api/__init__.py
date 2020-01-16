aa9a8300117acfed300b49bd7eededf2615500d7
Ted Hille <ted@galois.com>
2020-01-16 13:36:59 -0800

    expose report-job api

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -13,3 +13,4 @@ from app.api import versioned_resource  # noqa: E402, F401
 from app.api import feature_model_input  # noqa: E402, F401
 from app.api import feature_extraction_job  # noqa: E402, F401
 from app.api import feature_model  # noqa E402, F401
+from app.api import report_job  # noqa E402, F401
