1996289bbf90ceb83e6590613c6b29ac1fac5eda
Ted Hille <ted@galois.com>
2020-01-21 16:31:50 -0800

    update system config page test

--- client/src/pages/SystemConfigInput.test.tsx
+++ client/src/pages/SystemConfigInput.test.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Dispatch } from 'redux';
 
 import {
   ISystemConfigInputProps,
   SystemConfigInput,
 } from './SystemConfigInput';
-import { submitSystemConfigInput } from '../state/system';
+import {
+  submitSystemConfigInput,
+  updateSystemConfigInput,
+} from '../state/system';
 
 const genSystemConfigInputWrapper = () => {
     const props: ISystemConfigInputProps = {
+      isLoading: false,
+      errors: [],
+      dispatch: jest.fn() as Dispatch,
       workflowId: -1,
       createSystemConfig: jest.fn() as typeof submitSystemConfigInput,
+      updateSystemConfig: jest.fn() as typeof updateSystemConfigInput,
     };
 
     return mount(<SystemConfigInput {...props} />);
