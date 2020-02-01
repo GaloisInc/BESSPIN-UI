23cb081675715fec716e4e493f98c4e0b877fcfe
Ted Hille <ted@galois.com>
2020-01-31 17:06:58 -0800

    update system configuration input api documentation and add a unit test for workflow composition

--- server/tests/test_system_configuration_inputs.py
+++ server/tests/test_system_configuration_inputs.py
@@ -5,6 +5,7 @@ from app import create_app
 from app.models import (
     db,
     SystemConfigurationInput,
+    Workflow,
 )
 
 
@@ -22,13 +23,16 @@ class SystemConfigurationInputModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_unique_constraint(self):
+        wf = Workflow(label='wf')
+        db.session.add(wf)
+        db.session.commit()
 
         db.session.add(
             SystemConfigurationInput(
                 label='test sysconfig 1',
                 nixConfigFilename='foo.nix',
                 nixConfig='{ some: config }',
-                workflowId=1
+                workflowId=wf.workflowId
             )
         )
         db.session.add(
@@ -36,9 +40,32 @@ class SystemConfigurationInputModelTestCase(unittest.TestCase):
                 label='test sysconfig 2',
                 nixConfigFilename='bar.nix',
                 nixConfig='{ some: config }',
-                workflowId=1
+                workflowId=wf.workflowId
             )
         )
 
         with self.assertRaises(IntegrityError):
             db.session.commit()
+
+    def test_workflow_relationship(self):
+        wf = Workflow(label='wf')
+        db.session.add(wf)
+        db.session.commit()
+
+        test_label = 'test sysconfig'
+        db.session.add(
+            SystemConfigurationInput(
+                label=test_label,
+                nixConfigFilename='foo.nix',
+                nixConfig='{ some: config }',
+                workflowId=wf.workflowId
+            )
+        )
+
+        db.session.commit()
+
+        sc = SystemConfigurationInput.query.filter_by(label=test_label).first()
+
+        self.assertIsNotNone(sc)
+        self.assertIsNotNone(sc.workflow)
+        self.assertEqual(sc.workflow.label, 'wf')
