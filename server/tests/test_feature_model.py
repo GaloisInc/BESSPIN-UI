9e3b93e4e1a205d0a60e67e23a0deaed135b8f36
Ted Hille <ted@galois.com>
2020-02-06 20:15:52 -0800

    Move the JSON serialization/deserialization and computed properties into the database feature model

--- server/tests/test_feature_model.py
+++ server/tests/test_feature_model.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import json
 from uuid import uuid4
 
 from app import create_app
@@ -6,6 +8,16 @@ from app.models import (
     db,
     FeatureModel,
 )
+
+
+FM_JSON_TEST_FILEPATH = os.path.join(os.path.dirname(__file__), '../app/ui/examples/flute.fm.json')
+
+
+def load_test_fmjson() -> str:
+    data = ''
+    with open(FM_JSON_TEST_FILEPATH, 'r') as myfile:
+        data = ''.join(myfile.readlines())
+    return data
 
 
 class FeatureModelTestCase(unittest.TestCase):
@@ -28,3 +40,17 @@ class FeatureModelTestCase(unittest.TestCase):
         db.session.commit()
 
         self.assertEqual(len(FeatureModel.query.all()), 1)
+
+    def test_record_with_conftree(self):
+        uid = str(uuid4())
+        source = load_test_fmjson()
+
+        feature_model = FeatureModel(uid=uid, conftree=json.loads(source), filename='test.fm.json')
+
+        db.session.add(feature_model)
+        db.session.commit()
+
+        fm = FeatureModel.query.get(uid)
+
+        self.assertIsNotNone(fm)
+        self.assertEqual(fm.conftree['version']['base'], 1)
